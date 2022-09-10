0a27197fb2eba10cebcfde7f563afa02fa7c91a8
Edson Junior <efojunior25@gmail.com>
2022-09-10 06:39:45 -0300

    Aula 01

---
+++ Aula_01/script.js
@@ -0,0 +1,12 @@
+var nome = "Edson";
+
+var notaPriBim = 9;
+var notaSegBim = 7;
+var notaTerBim = 4;
+var notaQuaBim = 2;
+
+var notaFinal = (notaPriBim + notaSegBim + notaTerBim + notaQuaBim) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome + ", sua média foi " + notaFixada)
